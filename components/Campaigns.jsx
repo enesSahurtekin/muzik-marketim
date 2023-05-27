b91cfebc4d9b021ab558fd880151ec1148504176
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-05-27 19:22:24 +0300

    Update Campaigns.jsx

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -7,7 +7,7 @@ const CampaignItem = () => {
     <div className="bg-secondary flex-1 rounded-md py-5 px-[15px] flex items-center gap-x-4">
       <div className="relative md:w-44 md:h-44 w-36 h-36 after:content-['']   border-[5px] border-primary rounded-full overflow-hidden">
         <Image
-          src="/images/o1.jpg"
+          src="/images/img-7.png"
           alt=""
           layout="fill"
           className="hover:scale-105 transition-all"
@@ -16,13 +16,13 @@ const CampaignItem = () => {
         />
       </div>
       <div className="text-white">
-        <Title addClass="text-2xl">Tasty Thursdays</Title>
+        <Title addClass="text-2xl">tüm ürünlerde indirim</Title>
         <div className="font-dancing my-1">
           <span className="text-[40px]">20%</span>
           <span className="text-sm inline-block ml-1">Off</span>
         </div>
         <button className="btn-primary flex items-center gap-x-2">
-          Order Now <MdShoppingCart size={20} />
+          Sipariş ver <MdShoppingCart size={20} />
         </button>
       </div>
     </div>
