3a11460360af96f1b748df1f294e8bcf59176644
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-05-30 01:24:21 +0300

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -24,26 +24,26 @@ const Footer = () => {
       <div className="container mx-auto pt-16 pb-6">
         <div className="flex md:justify-between justify-center text-center flex-wrap md:gap-y-0 gap-y-6 ">
           <div className="md:flex-1">
-            <Title addClass="text-[30px]">Contact Us</Title>
+            <Title addClass="text-[30px]">Bize ulaşın</Title>
             <div className="flex flex-col gap-y-2 mt-3">
             <a href={footer?.location} target="_blank" rel="noreferrer">
                 <i className="fa fa-map-marker"></i>
-                <span className="inline-block ml-2">Location</span>
+                <span className="inline-block ml-2">KONUM</span>
                 </a>
               <a href={`tel:${footer?.phoneNumber}`}>
                 <i className="fa fa-phone"></i>
                 <span className="inline-block ml-2">
-                  Call +90 {footer?.phoneNumber}
+                  ara +90 {footer?.phoneNumber}
                 </span>
               </a>
               <a href={`mailto:${footer?.email}`}>
                 <i className="fa fa-envelope"></i>
-                <span className="inline-block ml-2">demo@gmail.com</span>
+                <span className="inline-block ml-2">MüzikMarketim@gmail.com</span>
                 </a>
             </div>
           </div>
           <div className="md:flex-1">
-            <Title addClass="text-[38px]">Feane</Title>
+            <Title addClass="text-[38px]">MüzikMarketim</Title>
             <p className="mt-3">{footer?.desc}</p>
             <div className="flex items-center justify-center mt-5 gap-x-2">
             {footer?.socialMedia?.map((item) => (
@@ -60,7 +60,7 @@ const Footer = () => {
             </div>
           </div>
           <div className="md:flex-1">
-            <Title addClass="text-[30px]">Opening Hours</Title>
+            <Title addClass="text-[30px]">Açılış Saatleri</Title>
             <div className="flex flex-col gap-y-2 mt-3">
               <div>
               <span className="inline-block ml-2">
@@ -76,7 +76,7 @@ const Footer = () => {
           </div>
         </div>
         <p className="text-center mt-10">
-          © 2022 All Rights Reserved By Free Html Templates
+          ©  2023 Tüm Hakları Saklıdır
         </p>
       </div>
     </div>
