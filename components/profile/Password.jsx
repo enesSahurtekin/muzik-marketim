e04aae731a3eb241f9390067b3f12ccd6953c519
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-05-30 01:35:38 +0300

    Update Password.jsx

--- components/profile/Password.jsx
+++ components/profile/Password.jsx
@@ -34,7 +34,7 @@ const Password = ({ user }) => {
       id: 1,
       name: "password",
       type: "password",
-      placeholder: "Your Password",
+      placeholder: "şifre",
       value: values.password,
       errorMessage: errors.password,
       touched: touched.password,
@@ -43,7 +43,7 @@ const Password = ({ user }) => {
       id: 2,
       name: "confirmPassword",
       type: "password",
-      placeholder: "Your Confirm Password",
+      placeholder: "şifreyi onaylayın",
       value: values.confirmPassword,
       errorMessage: errors.confirmPassword,
       touched: touched.confirmPassword,
@@ -51,7 +51,7 @@ const Password = ({ user }) => {
   ];
   return (
     <form className="lg:p-8 flex-1 lg:mt-0 mt-5" onSubmit={handleSubmit}>
-      <Title addClass="text-[40px]">Password</Title>
+      <Title addClass="text-[40px]">şifre</Title>
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-4">
         {inputs.map((input) => (
           <Input
@@ -63,7 +63,7 @@ const Password = ({ user }) => {
         ))}
       </div>
       <button className="btn-primary mt-4" type="submit">
-        Update
+        güncelle
       </button>
     </form>
   );
