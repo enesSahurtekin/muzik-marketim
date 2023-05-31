cf35582964fab51d3e378b241b46e495a4c5905a
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-06-01 00:11:17 +0300

    Update Products.jsx

--- components/admin/Products.jsx
+++ components/admin/Products.jsx
@@ -9,12 +9,12 @@ const Products = () => {
 
   const handleDelete = async (id) => {
     try {
-      if (confirm("Are you sure you want to delete this product?")) {
+      if (confirm("Bu ürünü silmek istediğinizden emin misiniz?")) {
         const res = await axios.delete(
           `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`
         );
         if (res.status === 200) {
-          toast.success("Product Deleted!");
+          toast.success("ürün silindi!");
           getProducts();
         }
       }
