99e8d1445b015c47ac9170888ea0dec27983b228
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-06-06 00:36:18 +0300

    Update login.js

--- schema/login.js
+++ schema/login.js
@@ -1,12 +1,12 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object({
-  email: Yup.string().required("Email gereklidir.").email("e-posta geçersiz."),
+  email: Yup.string().required("Email is required.").email("Email is invalid."),
   password: Yup.string()
-    .required("şifre geçersiz.")
-    .min(8, "Şifre en az 8 karakter olmalıdır.")
+    .required("Password is required.")
+    .min(8, "Password must be at least 8 characters.")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      "Şifre en az bir büyük harf, bir küçük harf, bir sayı ve bir özel karakter içermelidir."
+      /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/,
+      "Password must contain at least one uppercase, one lowercase, one number and one special character."
     ),
 });
