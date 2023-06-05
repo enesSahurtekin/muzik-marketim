6ec3aa64668698fb25e92d4db3b10d21985890d5
enesSahurtekin <124527934+enesSahurtekin@users.noreply.github.com>
2023-06-06 00:57:22 +0300

    Update register.js

--- schema/register.js
+++ schema/register.js
@@ -2,17 +2,13 @@ import * as Yup from "yup";
 
 export const registerSchema = Yup.object({
   fullName: Yup.string()
-    .required("Full name is required.")
-    .min(3, "Full name must be at least 3 characters."),
-  email: Yup.string().required("Email is required.").email("Email is invalid."),
+    .required("isim soyisim gerekli.")
+    .min(3, "isim soyisim en az 3 karekter olamlı."),
+  email: Yup.string().required("Email gerekli.").email("Email geçersiz."),
   password: Yup.string()
-    .required("Password is required.")
-    .min(8, "Password must be at least 8 characters.")
-    .matches(
-      /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/,
-      "Password must contain at least one uppercase, one lowercase, one number and one special character."
-    ),
+    .required("şifre gerekli.")
+    .min(8, "şifre en az 8 karekter olmalı."),
   confirmPassword: Yup.string()
-    .required("Confirm password is required.")
-    .oneOf([Yup.ref("password"), null], "Passwords must match."),
+    .required("şifrenin gerekli olduğnu onaylayın.")
+    .oneOf([Yup.ref("password"), null], "şifreler aynı olmalı."),
 });
